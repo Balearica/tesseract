46a70112e18697b643d0bebd6e206c1ccde869b4
Balearica <admin@scribeocr.com>
2024-09-28 21:29:01 -0700

    Added option to support exporting intermediate images to ScrollView rather than .pdf

--- src/ccstruct/debugpixa.h
+++ src/ccstruct/debugpixa.h
@@ -5,6 +5,8 @@
 
 #include <allheaders.h>
 
+#include "scrollview.h"
+
 namespace tesseract {
 
 // Class to hold a Pixa collection of debug images with captions and save them
@@ -30,11 +32,31 @@ public:
   // Adds the given pix to the set of pages in the PDF file, with the given
   // caption added to the top.
   void AddPix(const Image pix, const char *caption) {
+
+  #ifdef IMAGEMASK_SCROLLVIEW
+  #ifndef GRAPHICS_DISABLED
+    if (pix != nullptr)  {
+      int width = pixGetWidth(pix);
+      int height = pixGetHeight(pix);
+      auto *win = new ScrollView(caption, 0, 0, width, height, width, height);
+
+      if (pix != nullptr) {
+        win->Draw(pix, 0, 0);
+      }
+
+      win->UpdateWindow();
+      #ifdef SCROLLVIEW_NONINTERACTIVE
+      delete win;
+      #endif
+    }
+  #endif
+  #else
     int depth = pixGetDepth(pix);
     int color = depth < 8 ? 1 : (depth > 8 ? 0x00ff0000 : 0x80);
     Image pix_debug =
         pixAddSingleTextblock(pix, fonts_, caption, color, L_ADD_BELOW, nullptr);
     pixaAddPix(pixa_, pix_debug, L_INSERT);
+  #endif
   }
 
   // Sets the destination filename and enables images to be written to a PDF
